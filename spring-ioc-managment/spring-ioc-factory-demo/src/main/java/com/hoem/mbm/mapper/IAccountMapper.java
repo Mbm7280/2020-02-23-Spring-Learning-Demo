5778367849620d40c605505371fc45645aadb9f1
Mbm7280 <56158949+Mbm7280@users.noreply.github.com>
2020-02-23 20:51:37 +0800

    Create IAccountMapper.java

---
+++ spring-ioc-managment/spring-ioc-factory-demo/src/main/java/com/hoem/mbm/mapper/IAccountMapper.java
@@ -0,0 +1,23 @@
+package com.hoem.mbm.mapper;
+
+import java.util.List;
+
+/**
+* @Package: com.home.mbm.mapper
+* @ClassName: IAccountMapper
+* @Description: mapper 接口
+* @Author: mbm
+* @date: 2020/2/12 20:14
+* @Version: 1.0
+*/
+public interface IAccountMapper {
+
+    // 接口方法
+
+    /**
+     * select
+     * @return
+     */
+    List selectAll();
+
+}
