4747bc81dda7b4c2a08a71d7f2b82ea3c61567d6
Mbm7280 <56158949+Mbm7280@users.noreply.github.com>
2020-02-23 20:51:58 +0800

    Create IAccountService.java

---
+++ spring-ioc-managment/spring-ioc-factory-demo/src/main/java/com/hoem/mbm/service/IAccountService.java
@@ -0,0 +1,23 @@
+package com.hoem.mbm.service;
+
+import java.util.List;
+
+/**
+* @Package: com.home.mbm.service
+* @ClassName: IAccountService
+* @Description: service 接口
+* @Author: mbm
+* @date: 2020/2/12 20:15
+* @Version: 1.0
+*/
+public interface IAccountService {
+
+    // 接口方法
+
+    /**
+     * select
+     * @return
+     */
+    List selectAll();
+
+}
